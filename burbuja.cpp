d0a4ac5b09d935022135658a0ea0ae03e2fecc29
Alan90010 <dominzalan@gmail.com>
2026-09-21 16:57:40 -0600

    feat: agrega seguimiento de pasadas y contadores

--- burbuja.cpp
+++ burbuja.cpp
@@ -1,41 +1,58 @@
 #include <iostream>
 using namespace std;
 
+void mostrarArreglo(int numeros[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << numeros[i] << " ";
+    }
+    cout << endl;
+}
+
 void burbuja(int numeros[], int n) {
 
+    int comparaciones = 0;
+    int intercambios = 0;
+
     for (int i = 0; i < n - 1; i++) {
+
         for (int j = 0; j < n - 1 - i; j++) {
 
+            comparaciones++;
+
             if (numeros[j] > numeros[j + 1]) {
+
                 int temporal = numeros[j];
                 numeros[j] = numeros[j + 1];
                 numeros[j + 1] = temporal;
+
+                intercambios++;
             }
         }
+
+        cout << "Pasada " << i + 1 << ": ";
+        mostrarArreglo(numeros, n);
     }
+
+    cout << endl;
+    cout << "Elementos: " << n << endl;
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Intercambios: " << intercambios << endl;
 }
 
 int main() {
 
     int numeros[8] = {45, 12, 78, 3, 25, 10, 90, 33};
 
-    cout << "Arreglo original:" << endl;
-
-    for (int i = 0; i < 8; i++) {
-        cout << numeros[i] << " ";
-    }
+    cout << "Arreglo original: ";
+    mostrarArreglo(numeros, 8);
 
     cout << endl;
 
     burbuja(numeros, 8);
 
-    cout << "Arreglo ordenado:" << endl;
-
-    for (int i = 0; i < 8; i++) {
-        cout << numeros[i] << " ";
-    }
-
     cout << endl;
+    cout << "Arreglo ordenado: ";
+    mostrarArreglo(numeros, 8);
 
     return 0;
 }
