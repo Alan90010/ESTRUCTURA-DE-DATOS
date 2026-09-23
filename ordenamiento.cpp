cb37919fc082660a094084805387e5acc429d218
Alan90010 <dominzalan@gmail.com>
2026-09-23 10:41:14 -0600

    Implementa ordenamiento por insercion (insertionSort)

--- ordenamiento.cpp
+++ ordenamiento.cpp
@@ -11,12 +11,36 @@ void mostrarArreglo(int numeros[], int n)
     cout << endl;
 }
 
+void insertionSort(int numeros[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int clave = numeros[i];   // elemento a insertar
+        int j = i - 1;
+
+        // Desplaza a la derecha los elementos mayores que la clave
+        while (j >= 0 && numeros[j] > clave)
+        {
+            numeros[j + 1] = numeros[j];
+            j--;
+        }
+
+        // Inserta la clave en su posicion correcta
+        numeros[j + 1] = clave;
+    }
+}
+
 int main()
 {
     int numeros[] = {7, 3, 8, 2, 6, 4, 5};
     int n = 7;
 
     cout << "Arreglo original:" << endl;
+    mostrarArreglo(numeros, n);
+
+    insertionSort(numeros, n);
+
+    cout << "Arreglo ordenado:" << endl;
     mostrarArreglo(numeros, n);
 
     return 0;
