69ddbd07cbe9e305b0b52a11de0fa508a88f3490
Alan90010 <dominzalan@gmail.com>
2026-09-23 10:41:53 -0600

    Muestra cada insercion y registra comparaciones, desplazamientos e inserciones

--- ordenamiento.cpp
+++ ordenamiento.cpp
@@ -13,21 +13,49 @@ void mostrarArreglo(int numeros[], int n)
 
 void insertionSort(int numeros[], int n)
 {
+    int comparaciones = 0;
+    int desplazamientos = 0;
+    int inserciones = 0;
+
     for (int i = 1; i < n; i++)
     {
         int clave = numeros[i];   // elemento a insertar
         int j = i - 1;
 
+        cout << "Insertando: " << clave << endl;
+
         // Desplaza a la derecha los elementos mayores que la clave
-        while (j >= 0 && numeros[j] > clave)
+        while (j >= 0)
         {
-            numeros[j + 1] = numeros[j];
-            j--;
+            comparaciones++;              // se compara numeros[j] con clave
+
+            if (numeros[j] > clave)
+            {
+                numeros[j + 1] = numeros[j];
+                desplazamientos++;
+                j--;
+            }
+            else
+            {
+                break;                    // ya se encontro la posicion
+            }
         }
 
         // Inserta la clave en su posicion correcta
         numeros[j + 1] = clave;
+        inserciones++;
+
+        mostrarArreglo(numeros, n);
+        cout << endl;
     }
+
+    cout << "========================================" << endl;
+    cout << "       ESTADISTICAS" << endl;
+    cout << "========================================" << endl;
+    cout << endl;
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Desplazamientos: " << desplazamientos << endl;
+    cout << "Inserciones: " << inserciones << endl;
 }
 
 int main()
@@ -37,11 +65,9 @@ int main()
 
     cout << "Arreglo original:" << endl;
     mostrarArreglo(numeros, n);
+    cout << endl;
 
     insertionSort(numeros, n);
-
-    cout << "Arreglo ordenado:" << endl;
-    mostrarArreglo(numeros, n);
 
     return 0;
 }
